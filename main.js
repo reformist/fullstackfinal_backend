ae9fb8a2da21e5bea166dccdfe302d79ae806bb4
Nicholas Djedjos <72480037+reformist@users.noreply.github.com>
2024-05-09 01:19:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose')
 
 app.use(express.json());
